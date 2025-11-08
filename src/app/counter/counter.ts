b59152e5910eb52392373a2c32610a1d8dd92e23
fmkiko <fmkiko@gmail.com>
2025-11-08 19:14:33 +0100

    refactor: update store type in Counter component and remove AppState interface

--- src/app/counter/counter.ts
+++ src/app/counter/counter.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { AppState } from '../store/app.state';
 import { Observable } from 'rxjs';
 import { increment, decrement, reset } from '../store/counter/counter.actions';
 import { CommonModule } from '@angular/common';
+import { CounterState } from '../store/counter/counter.state';
 
 @Component({
   selector: 'app-counter',
@@ -14,7 +14,7 @@ import { CommonModule } from '@angular/common';
 export class Counter {
   count$: Observable<number>;
 
-  constructor(private store: Store<AppState>) {
+  constructor(private store: Store<{counter: CounterState}>) {
 
     this.count$ = this.store.select(state => state.counter.count);
   }
--- src/app/store/app.state.ts
+++
@@ -1,5 +0,0 @@
-import { CounterState } from './counter/counter.state';
-
-export interface AppState {
-  counter: CounterState;
-}
